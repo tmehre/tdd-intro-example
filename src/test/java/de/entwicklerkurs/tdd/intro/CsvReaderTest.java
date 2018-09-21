f53cb99654a6930c94c33582c8f301382fc272a8
M.Sc. Tobias Mehre <kontakt@entwicklerkurs.de>
2018-09-21 08:38:58 +0200

    Cycle 2 [RED]
    * new testcases drives development due to new requirement

--- src/test/java/de/entwicklerkurs/tdd/intro/CsvReaderTest.java
+++ src/test/java/de/entwicklerkurs/tdd/intro/CsvReaderTest.java
@@ -14,4 +14,12 @@ public class CsvReaderTest {
 		String value = csvReader.getValue("col1", 0);
 		assertThat(value, Matchers.is("val1"));
 	}
+	
+	@Test
+	public void readColumnByNameAtRowIndexTwoColumns() throws Exception {
+		CsvReader csvReader = new CsvReader();
+		csvReader.read("col1,col2\nval1,val2");
+		String value = csvReader.getValue("col2", 0);
+		assertThat(value, Matchers.is("val2"));
+	}
 }
