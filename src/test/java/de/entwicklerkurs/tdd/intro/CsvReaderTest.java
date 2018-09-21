6a80ecc2539da5415c60a427e20e9b7f9eb7d62a
M.Sc. Tobias Mehre <kontakt@entwicklerkurs.de>
2018-09-21 21:20:24 +0200

    Cycle 3 - Additional test cases

--- src/test/java/de/entwicklerkurs/tdd/intro/CsvReaderTest.java
+++ src/test/java/de/entwicklerkurs/tdd/intro/CsvReaderTest.java
@@ -22,15 +22,33 @@ public class CsvReaderTest {
 	}
 
 	@Test
-	public void readColumnByNameAtRowIndexTwoColumns() throws Exception {
+	public void readColumnByNameAtRowIndexZeroColumn2() throws Exception {
 		csvReader.read("col1,col2\nval1,val2");
 		assertColumnValueAtRow(0, "col2", "val2");
 	}
 	
 	@Test
-	public void readColumnByNameAtRowIndexTwoColumns2() throws Exception {
+	public void readColumnByNameAtRowIndexZeroColumn1() throws Exception {
+		csvReader.read("col1,col2\nval1,val2");
+		assertColumnValueAtRow(0, "col1", "val1");
+	}
+	
+	@Test
+	public void readColumnByNameAtRowIndex1Column2() throws Exception {
 		csvReader.read("col1,col2\nval11,val12\nval21,val22");
 		assertColumnValueAtRow(1, "col1", "val21");
+	}
+	
+	@Test
+	public void readColumnByNameAtRowIndex1Column1() throws Exception {
+		csvReader.read("col1,col2\nval11,val12\nval21,val22");
+		assertColumnValueAtRow(1, "col1", "val21");
+	}
+	
+	@Test
+	public void readColumnByNameAtRowIndex2Column2() throws Exception {
+		csvReader.read("col1,col2\nval11,val12\nval21,val22\nval31,val32");
+		assertColumnValueAtRow(2, "col2", "val32");
 	}
 
 	private void assertColumnValueAtRow(int rowIndex, String columnName, String expectedValue) {
