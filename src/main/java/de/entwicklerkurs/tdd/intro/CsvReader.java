a011c8a01280211d02463b30ca0d44a01bf51f6c
M.Sc. Tobias Mehre <kontakt@entwicklerkurs.de>
2018-09-21 08:39:57 +0200

    Cycle 2 [GREEN]
    * fixed values are sufficient to make test cases green

--- src/main/java/de/entwicklerkurs/tdd/intro/CsvReader.java
+++ src/main/java/de/entwicklerkurs/tdd/intro/CsvReader.java
@@ -1,13 +1,22 @@
 package de.entwicklerkurs.tdd.intro;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CsvReader {
 
+	private Map<String, List<String>> csvStructure;
+
 	public void read(String csvString) {
-		
+		csvStructure = new HashMap<String, List<String>>();
+		csvStructure.put("col1", Collections.singletonList("val1"));
+		csvStructure.put("col2", Collections.singletonList("val2"));
 	}
 
 	public String getValue(String columnName, int rowIndex) {
-		return "val1";
+		return csvStructure.get(columnName).get(rowIndex);
 	}
 
 }
