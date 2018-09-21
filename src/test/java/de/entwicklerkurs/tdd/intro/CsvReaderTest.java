587ee91169bfc1fdd1ffb1ca8da8170f1f1bc7a1
M.Sc. Tobias Mehre <kontakt@entwicklerkurs.de>
2018-09-21 08:41:17 +0200

    Cycle 2 [REFACTOR]
    * remove redundant reader constructor call

--- src/test/java/de/entwicklerkurs/tdd/intro/CsvReaderTest.java
+++ src/test/java/de/entwicklerkurs/tdd/intro/CsvReaderTest.java
@@ -3,21 +3,27 @@ package de.entwicklerkurs.tdd.intro;
 import static org.junit.Assert.assertThat;
 
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CsvReaderTest {
 
+	private CsvReader csvReader;
+
+	@Before
+	public void initReader() {
+		csvReader = new CsvReader();
+	}
+
 	@Test
 	public void readColumnByNameAtRowIndex() throws Exception {
-		CsvReader csvReader = new CsvReader();
 		csvReader.read("col1\nval1");
 		String value = csvReader.getValue("col1", 0);
 		assertThat(value, Matchers.is("val1"));
 	}
-	
+
 	@Test
 	public void readColumnByNameAtRowIndexTwoColumns() throws Exception {
-		CsvReader csvReader = new CsvReader();
 		csvReader.read("col1,col2\nval1,val2");
 		String value = csvReader.getValue("col2", 0);
 		assertThat(value, Matchers.is("val2"));
